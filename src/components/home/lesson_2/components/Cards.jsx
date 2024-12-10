ff4addfde043ac251158df1699c3dc5e3ed6c61c
hasanjon <hasanjon1224@gmail.com>
2024-12-11 02:31:24 +0500

    add

--- src/components/home/lesson_2/components/Cards.jsx
+++ src/components/home/lesson_2/components/Cards.jsx
@@ -5,34 +5,40 @@ const Cards = () => {
     <div className="container py-5">
       <section className="py-5 d-flex gap-1 align-items-center">
         <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
-          <div className="col-md-3 mb-5">
-            <div className="card" style={{ width: "" }}>
+          <div className="col mb-5">
+            <div className="card h-100" style={{ width: "" }}>
               <img
                 src="https://wallpapers.com/images/hd/4k-nature-moraine-lake-r66plwqa8m3z5reg.jpg"
                 className="card-img-top"
                 alt="card"
               />
               <div className="card-body text-center align-items-center p-4">
-                <h5 className="card-title">Fancy Product</h5>
+                <h5 className="card-title fw-bolder">Fancy Product</h5>
 
                 <p className="card-text">$40.00 - $80.00</p>
-                <div className="card-footer">
-                  <a href="#btn" className="btn btn-outline-dark">
+                <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                  <a href="#btn" className="btn btn-outline-dark mt-auto">
                     view options
                   </a>
                 </div>
               </div>
             </div>
           </div>
-          <div className="col-md-3 mb-5">
-            <div className="card" style={{ marginBottom: "" }}>
+          <div className="col mb-5">
+            <div className="card h-100" style={{ width: "" }}>
+              <div
+                className="badge bg-dark text-white position-absolute"
+                style={{ marginTop: "7px", right: "0.5rem" }}
+              >
+                Sale
+              </div>
               <img
                 src="https://wallpapers.com/images/hd/4k-nature-moraine-lake-r66plwqa8m3z5reg.jpg"
                 className="card-img-top"
                 alt="card"
               />
               <div className="card-body text-center align-items-center p-4">
-                <h5 className="card-title">Fancy Product</h5>
+                <h5 className="card-title fw-bolder">Fancy Product</h5>
                 <span className="d-flex text-center align-items-center justify-content-center">
                   <i className="bi bi-star-fill text-warning text-center"></i>
                   <i className="bi bi-star-fill text-warning text-center"></i>
@@ -41,41 +47,47 @@ const Cards = () => {
                   <i className="bi bi-star-fill text-warning text-center"></i>
                 </span>
                 <p className="card-text">$20.00 $18.00</p>
-                <div className="card-footer">
-                  <a href="#btn" className="btn btn-outline-dark">
+                <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                  <a href="#btn" className="btn btn-outline-dark mt-auto">
                     Add to cart
                   </a>
                 </div>
               </div>
             </div>
           </div>
-          <div className="col-md-3 mb-5">
-            <div className="card" style={{ width: "" }}>
+          <div className="col mb-5">
+            <div className="card h-100" style={{ width: "" }}>
+              <div
+                className="badge bg-dark text-white position-absolute"
+                style={{ marginTop: "7px", right: "0.5rem" }}
+              >
+                Sale
+              </div>
               <img
                 src="https://wallpapers.com/images/hd/4k-nature-moraine-lake-r66plwqa8m3z5reg.jpg"
                 className="card-img-top"
                 alt="card"
               />
               <div className="card-body text-center align-items-center p-4">
-                <h5 className="card-title">Fancy Product</h5>
+                <h5 className="card-title fw-bolder">Fancy Product</h5>
                 <p className="card-text">$50.00 $25.00</p>
-                <div className="card-footer">
-                  <a href="#btn" className="btn btn-outline-dark">
+                <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                  <a href="#btn" className="btn btn-outline-dark mt-auto">
                     Add to cart
                   </a>
                 </div>
               </div>
             </div>
           </div>
-          <div className="col-md-3 mb-5">
-            <div className="card" style={{ width: "" }}>
+          <div className="col mb-5">
+            <div className="card h-100" style={{ width: "" }}>
               <img
                 src="https://wallpapers.com/images/hd/4k-nature-moraine-lake-r66plwqa8m3z5reg.jpg"
                 className="card-img-top"
                 alt="card"
               />
               <div className="card-body text-center align-items-center p-4">
-                <h5 className="card-title">Popular item</h5>
+                <h5 className="card-title fw-bolder">Popular item</h5>
                 <span className="d-flex text-center align-items-center justify-content-center">
                   <i className="bi bi-star-fill text-warning text-center"></i>
                   <i className="bi bi-star-fill text-warning text-center"></i>
@@ -84,34 +96,39 @@ const Cards = () => {
                   <i className="bi bi-star-fill text-warning text-center"></i>
                 </span>
                 <p className="card-text">$40.00</p>
-                <div className="card-footer">
-                  Name
-                  <a href="#btn" className="btn btn-outline-dark">
-                    add to cart
+                <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                  <a href="#btn" className="btn btn-outline-dark mt-auto">
+                    Add to cart
                   </a>
                 </div>
               </div>
             </div>
           </div>
-          <div className="col-md-3">
-            <div className="card" style={{ width: "" }}>
+          <div className="col mb-5">
+            <div className="card h-100" style={{ width: "" }}>
+              <div
+                className="badge bg-dark text-white position-absolute"
+                style={{ marginTop: "7px", right: "0.5rem" }}
+              >
+                Sale
+              </div>
               <img
                 src="https://wallpapers.com/images/hd/4k-nature-moraine-lake-r66plwqa8m3z5reg.jpg"
                 className="card-img-top"
                 alt="card"
               />
               <div className="card-body text-center align-items-center p-4">
-                <h5 className="card-title">Sale item</h5>
+                <h5 className="card-title fw-bolder">Sale item</h5>
                 <p className="card-text">$50.00 $25.00</p>
-                <div className="card-footer">
-                  <a href="#btn" className="btn btn-outline-dark">
+                <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                  <a href="#btn" className="btn btn-outline-dark mt-auto">
                     add to cart
                   </a>
                 </div>
               </div>
             </div>
           </div>
-          <div className="col-md-3">
+          <div className="col mb-5">
             <div className="card h-100" style={{ width: "" }}>
               <img
                 src="https://wallpapers.com/images/hd/4k-nature-moraine-lake-r66plwqa8m3z5reg.jpg"
@@ -119,25 +136,31 @@ const Cards = () => {
                 alt="card"
               />
               <div className="card-body text-center align-items-center p-4">
-                <h5 className="card-title">Fancy Product</h5>
+                <h5 className="card-title fw-bolder">Fancy Product</h5>
                 <p className="card-text">$120.00 - $280.00</p>
-                <div className="card-footer">
-                  <a href="#btn" className="btn btn-outline-dark">
+                <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                  <a href="#btn" className="btn btn-outline-dark mt-auto">
                     view options
                   </a>
                 </div>
               </div>
             </div>
           </div>
-          <div className="col-md-3">
+          <div className="col mb-5">
             <div className="card h-100" style={{ width: "" }}>
+              <div
+                className="badge bg-dark text-white position-absolute"
+                style={{ marginTop: "7px", right: "0.5rem" }}
+              >
+                Sale
+              </div>
               <img
                 src="https://wallpapers.com/images/hd/4k-nature-moraine-lake-r66plwqa8m3z5reg.jpg"
                 className="card-img-top"
                 alt="card"
               />
               <div className="card-body text-center align-items-center p-4">
-   NclassName             <h5 className="card-title">Special item</h5>
+                <h5 className="card-title fw-bolder">Special item</h5>
                 <span className="d-flex text-center align-items-center justify-content-center">
                   <i className="bi bi-star-fill text-warning text-center"></i>
                   <i className="bi bi-star-fill text-warning text-center"></i>
@@ -146,15 +169,15 @@ const Cards = () => {
                   <i className="bi bi-star-fill text-warning text-center"></i>
                 </span>
                 <p className="card-text">$20.00 $18.00</p>
-                <div className="card-footer">
-                  <a href="#btn" class="btn btn-outline-dark">
+                <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                  <a href="#btn" class="btn btn-outline-dark mt-auto">
                     Add to cart
                   </a>
                 </div>
               </div>
             </div>
           </div>
-          <div className="col-md-3">
+          <div className="col mb-5">
             <div className="card h-100" style={{ width: "" }}>
               <img
                 src="https://wallpapers.com/images/hd/4k-nature-moraine-lake-r66plwqa8m3z5reg.jpg"
@@ -171,8 +194,8 @@ const Cards = () => {
                   <i className="bi bi-star-fill text-warning text-center"></i>
                 </span>
                 <p className="card-text">$40.00</p>
-                <div className="card-footer">
-                  <a href="#btn" className="btn btn-outline-dark">
+                <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                  <a href="#btn" className="btn btn-outline-dark mt-auto">
                     Add to cart
                   </a>
                 </div>
